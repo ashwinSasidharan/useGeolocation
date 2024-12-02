7120a6e1e8c540a61e5582c325e25e368ed80796
ashwinSasidharan <ashwinsasidharanverse@gmail.com>
2024-12-02 13:35:24 +0530

    my solution

--- useGeolocation.js
+++ useGeolocation.js
@@ -1,28 +1,68 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
-export function useGeolocation(count) {
-  const [isLoading, setIsLoading] = useState(false);
+function useGeolocation(errorUpdater, loadingUpdater, invoker) {
   const [position, setPosition] = useState({});
+  useEffect(
+    function () {
+      if (!navigator.geolocation)
+        return errorUpdater?.("Your browser does not support geolocation");
+
+      loadingUpdater(true);
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          setPosition({
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude,
+          });
+          loadingUpdater?.(false);
+        },
+        (error) => {
+          errorUpdater?.(error.message);
+          loadingUpdater?.(false);
+        }
+      );
+    },
+    [invoker, errorUpdater, loadingUpdater]
+  );
+
+  return position;
+}
+
+export default function Challenge() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countClicks, setCountClicks] = useState(0);
+
   const [error, setError] = useState(null);
 
-  useEffect(function () {
-    if (!navigator.geolocation)
-      return setError("Your browser does not support geolocation");
+  const position = useGeolocation(setError, setIsLoading, countClicks);
+  const { lat, lng } = position;
 
-    setIsLoading(true);
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-        setIsLoading(false);
-      },
-      (error) => {
-        setError(error.message);
-        setIsLoading(false);
-      }
-    );
-  }, []);
-  return count > 0 ? [isLoading, position, error] : [false, {}, null];
+  function getPosition() {
+    setCountClicks((count) => count + 1);
+  }
+
+  return (
+    <div>
+      <button onClick={getPosition} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && countClicks > 0 && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
 }
